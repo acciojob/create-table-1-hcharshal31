9f3e7e1efffe1318f9c7a013d9787b5c1b68eba0
Harshal Chavan <91681985+hcharshal31@users.noreply.github.com>
2025-01-08 17:50:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 
 
 function insert_Row() {
- let table = document.getElementById("sampleTable");
-table.innerHTML += `
-	<tr><td>Row3 cell1</td> 
-	<td>Row3 cell2</td></tr>
-	`;
+	let table = document.getElementById("sampleTable");
+	let newRow = document.createElement("tr");
+	let cell1 = document.createElement("td");
+	let cell2 = document.createElement("td");
+
+	cell1.textContent = "New Cell1";
+	cell2.textContent = "New Cell2";
+
+	newRow.appendChild(cell1);
+	newRow.appendChild(cell2);
+	
+	table.prepend(newRow);
 }
