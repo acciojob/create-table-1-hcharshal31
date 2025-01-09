81c7324f1935f50a4e7367afd0ca99d7679d10c2
Harshal Chavan <91681985+hcharshal31@users.noreply.github.com>
2025-01-09 18:59:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
+// let insert = document.getElementById
+
 function insert_Row() {
-    let table = document.getElementById("sampleTable");
-    let newRow = document.createElement("tr");
-    let cell1 = document.createElement("td");
-    let cell2 = document.createElement("td");
-
-    cell1.textContent = "New Cell1";
-    cell2.textContent = "New Cell2";
-
-    newRow.appendChild(cell1);
-    newRow.appendChild(cell2);
-    
-    table.prepend(newRow);
+    let table = document.getElementById('sampleTable');
+            let newRow = table.insertRow(0); // Add after headers but before the first data row
+            let cell1 = newRow.insertCell(0);
+            let cell2 = newRow.insertCell(1);
+            cell1.innerHTML = "New Cell1";
+            cell2.innerHTML = "New Cell2";
 }
